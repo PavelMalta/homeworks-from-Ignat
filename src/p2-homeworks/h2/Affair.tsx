04d7ff1068949a75541775533159f1df1c9de14e
PavelMalta <shamashov87@mail.ru>
2021-01-17 15:47:27 +0300

    add CSS in HW2

--- src/p2-homeworks/h2/Affair.tsx
+++ src/p2-homeworks/h2/Affair.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {AffairType} from "./HW2";
+import s from "./Affairs.module.css"
 
 type AffairPropsType = {
     // key не нужно типизировать
@@ -8,16 +9,30 @@ type AffairPropsType = {
 }
 
 function Affair(props: AffairPropsType) {
-    const deleteCallback = () => {props.deleteAffairCallback(props.affair._id)};// need to fix
+    const deleteCallback = () => {
+        props.deleteAffairCallback(props.affair._id)
+    };// need to fix
+
+    let color = s.status
+    if (props.affair.priority === "high") {
+        color = s.high
+    } else if (props.affair.priority === "middle") {
+        color = s.middle
+    } else if (props.affair.priority === "low") {
+        color = s.low
+    }
 
     return (
-        <div>
-            <span>
-            {props.affair.name}
-            </span>
-            <button onClick={deleteCallback}>X</button>
+        <div className={s.item}>
+            <div className={s.affair}>{props.affair.name}</div>
+            <div className={color}>{props.affair.priority}</div>
+            <div className={s.button}>
+                <button onClick={deleteCallback}>X</button>
+            </div>
         </div>
     );
 }
 
 export default Affair;
+
+
