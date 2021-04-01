c8ae28c03fbd5363cb401995b3e4764360484039
PavelMalta <shamashov87@mail.ru>
2021-04-01 16:06:53 +0300

    add Routers and NavLinks in HW5

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
-
+import { NavLink } from 'react-router-dom'
+import {PATH} from "./Routes";
+import s from"./Header.module.css"
 function Header() {
     return (
-        <div>
-            // add NavLinks
-
+        <div className={s.header}>
+            <NavLink to={PATH.PRE_JUNIOR} className={s.page} activeClassName={s.active}>Pre-Junior</NavLink>
+            <NavLink to={PATH.JUNIOR} className={s.page} activeClassName={s.active}>Pre-Junior</NavLink>
+            <NavLink to={PATH.JUNIOR_PLUS} className={s.page} activeClassName={s.active}>Junior-Plus</NavLink>
         </div>
     )
 }
