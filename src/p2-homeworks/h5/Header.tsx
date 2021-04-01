a605da230a4dcc7f94c16f6875fa5760fead507a
PavelMalta <shamashov87@mail.ru>
2021-04-01 17:50:24 +0300

    add popup menu in Header component, HW5

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -8,6 +8,7 @@ function Header() {
             <NavLink to={PATH.PRE_JUNIOR} className={s.page} activeClassName={s.active}>Pre-Junior</NavLink>
             <NavLink to={PATH.JUNIOR} className={s.page} activeClassName={s.active}>Pre-Junior</NavLink>
             <NavLink to={PATH.JUNIOR_PLUS} className={s.page} activeClassName={s.active}>Junior-Plus</NavLink>
+            <div className={s.menu}>menu</div>
         </div>
     )
 }
