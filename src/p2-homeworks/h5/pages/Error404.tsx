0e50c756cc6903c2f0eedc2447e816336fdafce9
PavelMalta <shamashov87@mail.ru>
2021-04-01 17:05:05 +0300

    add styles 404 page, HW5

--- src/p2-homeworks/h5/pages/Error404.tsx
+++ src/p2-homeworks/h5/pages/Error404.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
+import s from './Error404.module.css'
 
 function Error404() {
     return (
-        <div>
-            <div>404</div>
-            <div>Page not found!</div>
-            <div>—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>
+        <div className={s.error}>
+            <div className={s.area}>404</div>
+            <div className={`${s.area} ${s.description}`}>Page not found!</div>
         </div>
     )
 }
