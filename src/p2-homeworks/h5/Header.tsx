d60e79177d0a01aba260823ee0a8fbca119f241f
PavelMalta <shamashov87@mail.ru>
2021-04-08 14:44:30 +0300

    fixed SuperSelect component, HW7

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -6,7 +6,7 @@ function Header() {
     return (
         <div className={s.header}>
             <NavLink to={PATH.PRE_JUNIOR} className={s.page} activeClassName={s.active}>Pre-Junior</NavLink>
-            <NavLink to={PATH.JUNIOR} className={s.page} activeClassName={s.active}>Pre-Junior</NavLink>
+            <NavLink to={PATH.JUNIOR} className={s.page} activeClassName={s.active}>Junior</NavLink>
             <NavLink to={PATH.JUNIOR_PLUS} className={s.page} activeClassName={s.active}>Junior-Plus</NavLink>
             <div className={s.menu}>menu</div>
         </div>
